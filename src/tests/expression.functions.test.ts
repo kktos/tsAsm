d6036611a1d9e963dad436385646e790af14a60c
kktos <tm.barral@gmail.com>
2025-12-04 18:26:14 +0100

    feat: add tests for lobyte/hibyte

--- src/tests/expression.functions.test.ts
+++ src/tests/expression.functions.test.ts
@@ -93,5 +93,25 @@ describe("ExpressionEvaluator", () => {
 
 			expect(evaluator.evaluate(tokenize('.IIF(50-50, "true", "false")'), { pc: 0 })).toBe("false");
 		});
+
+		it("should evaluate .LOBYTE() to get the low byte of a 16-bit value", () => {
+			const { evaluator, tokenize } = setup();
+			let tokens = tokenize(".LOBYTE($1234)");
+			let result = evaluator.evaluateAsNumber(tokens, { pc: 0 });
+			expect(result).toBe(0x34);
+			tokens = tokenize(".LOBYTE($22)");
+			result = evaluator.evaluateAsNumber(tokens, { pc: 0 });
+			expect(result).toBe(0x22);
+		});
+
+		it("should evaluate .HIBYTE() to get the high byte of a 16-bit value", () => {
+			const { evaluator, tokenize } = setup();
+			let tokens = tokenize(".HIBYTE($1234)");
+			let result = evaluator.evaluateAsNumber(tokens, { pc: 0 });
+			expect(result).toBe(0x12);
+			tokens = tokenize(".HIBYTE($34)");
+			result = evaluator.evaluateAsNumber(tokens, { pc: 0 });
+			expect(result).toBe(0x00);
+		});
 	});
 });
