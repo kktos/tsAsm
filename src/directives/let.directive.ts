8849722749b1783fde5036ace4c64423d7cca3f0
kktos <tm.barral@gmail.com>
2025-12-07 17:36:50 +0100

    feat: .LET

---
+++ src/directives/let.directive.ts
@@ -0,0 +1,54 @@
+import type { ScalarToken, Token } from "../lexer/lexer.class";
+import type { Assembler } from "../polyasm";
+import type { StreamState } from "../polyasm.types";
+import type { SymbolValue } from "../symbol.class";
+import { hasNoMoreThanOne } from "../utils/array.utils";
+import type { DirectiveContext, IDirective } from "./directive.interface";
+
+export class LetDirective implements IDirective {
+	public isBlockDirective = false;
+	public isRawDirective = false;
+
+	public handlePassOne(directive: ScalarToken, assembler: Assembler, _context: DirectiveContext): void {
+		this.handleLet(directive, assembler);
+	}
+
+	public handlePassTwo(directive: ScalarToken, assembler: Assembler, _context: DirectiveContext): void {
+		this.handleLet(directive, assembler);
+	}
+
+	private handleLet(directive: ScalarToken, assembler: Assembler) {
+		const lineTokens = assembler.parser.getExpressionTokens(directive);
+		if (!hasNoMoreThanOne<Token>(lineTokens, (token: Token) => token.type === "OPERATOR" && token.value === "="))
+			throw `.LET expression only allows operator "==" to test equality`;
+
+		const index = lineTokens.findIndex((token) => token.type === "OPERATOR" && token.value === "=");
+
+		let name: SymbolValue = "";
+		const nameTokens = lineTokens.slice(0, index);
+		if (nameTokens.length === 1 && nameTokens[0]?.type === "IDENTIFIER") name = nameTokens[0].value;
+		else
+			name = assembler.expressionEvaluator.evaluate(nameTokens, {
+				pc: assembler.currentPC,
+				allowForwardRef: assembler.pass === 1,
+				currentGlobalLabel: assembler.getLastGlobalLabel(),
+				macroArgs: (assembler.parser.tokenStreamStack[assembler.parser.tokenStreamStack.length - 1] as StreamState).macroArgs,
+			});
+		if (typeof name !== "string") throw "Expected a string for symbol name";
+
+		if (!assembler.lexer.isValidIdentifier(name)) throw `Invalid identifier for symbol name : ${name}`;
+
+		const expressionTokens = lineTokens.slice(index + 1);
+		const value = assembler.expressionEvaluator.evaluate(expressionTokens, {
+			pc: assembler.currentPC,
+			allowForwardRef: assembler.pass === 1,
+			currentGlobalLabel: assembler.getLastGlobalLabel(),
+			macroArgs: (assembler.parser.tokenStreamStack[assembler.parser.tokenStreamStack.length - 1] as StreamState).macroArgs,
+		});
+
+		assembler.lister.symbol(name, value);
+
+		if (assembler.pass === 1) assembler.symbolTable.addSymbol(name, value);
+		else assembler.symbolTable.setSymbol(name, value);
+	}
+}
---
+++ src/tests/let.directive.test.ts
@@ -0,0 +1,117 @@
+import { describe, expect, it } from "vitest";
+import { Assembler } from "../polyasm";
+
+// Minimal fake CPU handler
+const fakeCPU = {
+	cpuType: "6502" as const,
+	isInstruction: () => false,
+	resolveAddressingMode: () => ({
+		mode: "",
+		opcode: 0,
+		bytes: 0,
+		resolvedAddress: 0,
+	}),
+	encodeInstruction: () => [],
+	getPCSize: () => 8,
+};
+
+function makeAssembler() {
+	const asm = new Assembler(fakeCPU, { fullpath: "", readSourceFile: () => "", readBinaryFile: () => [] });
+	return { asm };
+}
+
+describe(".LET directive", () => {
+	describe("static name", () => {
+		it("should define a symbol with a value", () => {
+			const { asm } = makeAssembler();
+			const src = ".LET foo = 123";
+			asm.assemble(src);
+			const symbol = asm.symbolTable.lookupSymbol("foo");
+			expect(symbol).toBe(123);
+		});
+
+		it("should allow forward references", () => {
+			const { asm } = makeAssembler();
+			const src = `
+			.LET foo = bar
+			bar = 10
+		`;
+			asm.assemble(src);
+			const symbol = asm.symbolTable.lookupSymbol("foo");
+			expect(symbol).toBe(10);
+		});
+
+		it("should throw an error when re-defining a constant with .LET", () => {
+			const { asm } = makeAssembler();
+			const src = `
+			foo = 10
+			.LET foo = 20
+		`;
+			expect(() => asm.assemble(src)).toThrow(/ERROR: PASymbol global::FOO redefined./);
+		});
+
+		it("should throw an error when re-defining a .LET variable as a constant", () => {
+			const { asm } = makeAssembler();
+			const src = `
+			.LET foo = 10
+			foo = 20
+		`;
+			expect(() => asm.assemble(src)).toThrow(/ERROR: PASymbol global::FOO redefined./);
+		});
+
+		it("should handle complex expressions", () => {
+			const { asm } = makeAssembler();
+			const src = `
+			.LET foo = 10 + 5
+			.LET bar = foo * 2
+		`;
+			asm.assemble(src);
+			const symbolBar = asm.symbolTable.lookupSymbol("bar");
+			expect(symbolBar).toBe(30);
+		});
+
+		it("should handle strings", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET name = "hello"`;
+			asm.assemble(src);
+			const symbolName = asm.symbolTable.lookupSymbol("name");
+			expect(symbolName).toBe("hello");
+		});
+	});
+
+	describe("dynamic name", () => {
+		it("should handle name as strings concatenation", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET ("na")+("me") = "hello"`;
+			asm.assemble(src);
+			const symbolName = asm.symbolTable.lookupSymbol("name");
+			expect(symbolName).toBe("hello");
+		});
+
+		it("should not allow operator = in name expression", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET ("na")+("me"="") = "hello"`;
+			expect(() => asm.assemble(src)).toThrow(/.LET expression only allows operator "==" to test equality/);
+		});
+
+		it("should allow operator == in name expression", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET ("na")+("me"=="") = "hello"`;
+			asm.assemble(src);
+			const symbolName = asm.symbolTable.lookupSymbol("na0");
+			expect(symbolName).toBe("hello");
+		});
+
+		it("should allow only valid identifers in name expression", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET "123" = "hello"`;
+			expect(() => asm.assemble(src)).toThrow(/Invalid identifier for symbol name : 123/);
+		});
+
+		it("should allow only valid identifers in name expression 2", () => {
+			const { asm } = makeAssembler();
+			const src = `.LET "n@me" = "hello"`;
+			expect(() => asm.assemble(src)).toThrow(/Invalid identifier for symbol name : n@me/);
+		});
+	});
+});
