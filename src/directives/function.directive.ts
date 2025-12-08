43e8dda2cb0d9e1f8f2a938d5ab20146ca80730c
kktos <tm.barral@gmail.com>
2025-12-08 18:22:39 +0100

    fix: function pass macroArgs to new stream

--- src/directives/function.directive.ts
+++ src/directives/function.directive.ts
@@ -11,8 +11,8 @@ export class FunctionDirective implements IDirective {
 		if (nameToken?.type !== "IDENTIFIER") throw new Error(`line ${directive.line}: Expected function name after .FUNCTION directive.`);
 
 		// Define the function name as a label in the current scope.
-		if (assembler.pass === 1) assembler.symbolTable.addSymbol(nameToken.value, context.pc);
-		else assembler.symbolTable.setSymbol(nameToken.value, context.pc);
+		if (assembler.pass === 1) assembler.symbolTable.defineConstant(nameToken.value, context.pc);
+		else assembler.symbolTable.updateSymbol(nameToken.value, context.pc);
 
 		// assembler.logger.log(`Defined function label '${nameToken.value}' @ $${context.pc.toString(16)}`);
 		assembler.lister.directive(directive, nameToken.raw as string);
@@ -21,7 +21,12 @@ export class FunctionDirective implements IDirective {
 		assembler.symbolTable.pushScope(scopeId);
 
 		const body = assembler.parser.getDirectiveBlockTokens(directive.value);
-		assembler.parser.pushTokenStream({ cacheName: scopeId, newTokens: assembler.pass === 1 ? body : [], onEndOfStream: () => assembler.symbolTable.popScope() });
+		assembler.parser.pushTokenStream({
+			cacheName: scopeId,
+			newTokens: assembler.pass === 1 ? body : [],
+			onEndOfStream: () => assembler.symbolTable.popScope(),
+			macroArgs: context.macroArgs,
+		});
 	}
 
 	public handlePassOne(directive: ScalarToken, assembler: Assembler, context: DirectiveContext): void {
