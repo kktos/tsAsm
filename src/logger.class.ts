07094c3049dcdbe3b3d6b6092dc3145ca011aa43
kktos <tm.barral@gmail.com>
2025-12-09 10:03:33 +0100

    feat: add cache to Logger

--- src/logger.class.ts
+++ src/logger.class.ts
@@ -2,9 +2,19 @@ export class Logger {
 	public enabled = true;
 	private warnings: string[] = [];
 	private errors: string[] = [];
+	private logs: unknown[][] = [];
 
 	constructor(enabled = true) {
 		this.enabled = enabled;
+	}
+
+	public cache(...args: unknown[]) {
+		if (this.enabled) this.logs.push(args);
+	}
+
+	public flush() {
+		if (this.enabled && this.logs.length > 0) for (const args of this.logs) console.log(...args);
+		this.logs = [];
 	}
 
 	public log(message: string): void {
@@ -24,6 +34,7 @@ export class Logger {
 		return {
 			warnings: this.warnings,
 			errors: this.errors,
+			log: this.logs,
 		};
 	}
 }
