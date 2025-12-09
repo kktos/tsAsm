692a8964f1b60c1ab60d3077a62bd38b96fe0c14
kktos <tm.barral@gmail.com>
2025-12-09 15:18:17 +0100

    feat: Function use restoreAndPushScope

--- src/directives/function.directive.ts
+++ src/directives/function.directive.ts
@@ -18,13 +18,13 @@ export class FunctionDirective implements IDirective {
 		assembler.lister.directive(directive, nameToken.raw as string);
 
 		const scopeId = `@@function_${assembler.symbolTable.getSymbolFullPath(nameToken.value)}`;
-		assembler.symbolTable.pushScope(scopeId);
+		assembler.symbolTable.restoreAndPushScope(scopeId);
 
 		const body = assembler.parser.getDirectiveBlockTokens(directive.value);
 		assembler.parser.pushTokenStream({
 			cacheName: scopeId,
 			newTokens: assembler.pass === 1 ? body : [],
-			onEndOfStream: () => assembler.symbolTable.popScope(),
+			onEndOfStream: () => assembler.symbolTable.popScope({ wannaSave: assembler.pass === 1 }),
 			macroArgs: context.macroArgs,
 		});
 	}
